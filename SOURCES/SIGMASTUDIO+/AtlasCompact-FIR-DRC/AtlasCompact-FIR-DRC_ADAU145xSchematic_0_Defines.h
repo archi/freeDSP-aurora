cc747bb6734655d945cc27d37e00d32d56c14c9c
archi <github@mailhell.seb7.de>
2026-08-19 02:54:40 +0200

    My Plugin (in a branch of its own, not intended for upstream)
    I lost this once, no interest in losing it again...
    
    ATTENTION: The SigmaStudio+ code is utterly untested!

---
+++ SOURCES/SIGMASTUDIO+/AtlasCompact-FIR-DRC/AtlasCompact-FIR-DRC_ADAU145xSchematic_0_Defines.h
@@ -0,0 +1,24 @@
+/* 
+ * File:	C:\Users\ich\Documents\freeDSP-aurora\SOURCES\SIGMASTUDIO+\AtlasCompact-FIR-DRC\AtlasCompact-FIR-DRC_ADAU145xSchematic_0_Defines.h
+ * Created:	Wednesday, 19 August 2026 0:46 AM
+ * Description:	Schematic default download data definitions.
+ * 
+ * This software is distributed in the hope that it will be useful, 
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * 
+ * This software may only be used to program products purchased from 
+ * Analog Devices for incorporation by you into audio products that 
+ * are intended for resale to audio product end users.This software 
+ * may not be distributed whole or in any part to third parties. 
+ * 
+ * Copyright © 2026 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __ATLASCOMPACT-FIR-DRC_ADAU145XSCHEMATIC_0_DEFINES_H__
+#define __ATLASCOMPACT-FIR-DRC_ADAU145XSCHEMATIC_0_DEFINES_H__
+
+#define BufferSize_ADAU145XSCHEMATIC_0			29170
+#define NumTransactions_ADAU145XSCHEMATIC_0		70
+
+#endif
